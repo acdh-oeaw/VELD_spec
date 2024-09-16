4c8ac024b11f3e750e2f8082d57f59c1a1a0b8ef
SteffRhes <steffrhes@pm.me>
2024-09-16 18:50:18 +0200

    extend with barebone valid and invalid test cases

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -14,14 +14,36 @@ class TestVeldMetadata(unittest.TestCase):
     def validate_veldmetadata_file(self, veld_metadata_file_path):
         with open(TEST_FILES_PATH + veld_metadata_file_path, "r") as veld_metadata_file:
             veld_metadata = yaml.safe_load(veld_metadata_file)
-            try:
-                validate(veld_metadata)
-            except jsonschema.exceptions.ValidationError as err:
-                self.fail(err)
+            validate(veld_metadata)
     
-    def test_1(self):
-        self.validate_veldmetadata_file("test_1.yaml")
+    def validate_veldmetadata_file_valid(self, veld_metadata_file_path):
+        try:
+            self.validate_veldmetadata_file(veld_metadata_file_path)
+        except jsonschema.exceptions.ValidationError as err:
+            self.fail(err)
+                
+    def validate_veldmetadata_file_invalid(self, veld_metadata_file_path):
+        try:
+            self.validate_veldmetadata_file(veld_metadata_file_path)
+            self.fail("this should not be valid")
+        except jsonschema.exceptions.ValidationError as err:
+            pass
     
-    def test_2(self):
-        self.validate_veldmetadata_file("test_2.yaml")
+    def test_chain_barebone_valid(self):
+        self.validate_veldmetadata_file_valid("test_chain_barebone_valid.yaml")
+    
+    def test_chain_barebone_invalid(self):
+        self.validate_veldmetadata_file_invalid("test_chain_barebone_invalid.yaml")
+    
+    def test_code_barebone_valid(self):
+        self.validate_veldmetadata_file_valid("test_code_barebone_valid.yaml")
+    
+    def test_code_barebone_invalid(self):
+        self.validate_veldmetadata_file_invalid("test_code_barebone_invalid.yaml")
+    
+    def test_data_barebone_valid(self):
+        self.validate_veldmetadata_file_valid("test_data_barebone_valid.yaml")
+    
+    def test_data_barebone_invalid(self):
+        self.validate_veldmetadata_file_invalid("test_data_barebone_invalid.yaml")
         
