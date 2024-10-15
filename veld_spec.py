e287bf85c31bb1a49763abe4d6c464060261b269
SteffRhes <steffrhes@pm.me>
2024-10-15 21:13:51 +0200

    integrate schema

--- veld_spec.py
+++ veld_spec.py
@@ -1,19 +1,117 @@
-import json
-
 import jsonschema
+
+SCHEMA_DICT = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "definitions": {
+        "about": {
+            "type": "object",
+            "properties": {
+                "description": {
+                    "type": "null"
+                },
+                "topics": {
+                    "type": "null"
+                }
+            },
+            "additionalProperties": False
+        }
+    },
+    "oneOf": [
+        {
+            "type": "object",
+            "properties": {
+                "x-veld": {
+                    "type": "object",
+                    "properties": {
+                        "data": {
+                            "oneOf": [
+                                {
+                                    "$ref": "#/definitions/about"
+                                },
+                                {
+                                    "type": "null"
+                                }
+                            ]
+                        }
+                    },
+                    "required": [
+                        "data"
+                    ],
+                    "additionalProperties": False
+                }
+            },
+            "required": [
+                "x-veld"
+            ],
+            "additionalProperties": False
+        },
+        {
+            "type": "object",
+            "properties": {
+                "x-veld": {
+                    "oneOf": [
+                        {
+                            "type": "object",
+                            "properties": {
+                                "code": {
+                                    "oneOf": [
+                                        {
+                                            "$ref": "#/definitions/about"
+                                        },
+                                        {
+                                            "type": "null"
+                                        }
+                                    ]
+                                }
+                            },
+                            "required": [
+                                "code"
+                            ],
+                            "additionalProperties": False
+                        },
+                        {
+                            "type": "object",
+                            "properties": {
+                                "chain": {
+                                    "oneOf": [
+                                        {
+                                            "$ref": "#/definitions/about"
+                                        },
+                                        {
+                                            "type": "null"
+                                        }
+                                    ]
+                                }
+                            },
+                            "required": [
+                                "chain"
+                            ],
+                            "additionalProperties": False
+                        }
+                    ]
+                },
+                "services": {
+                    "type": "null"
+                }
+            },
+            "required": [
+                "x-veld",
+                "services"
+            ],
+            "additionalProperties": False
+        }
+    ]
+}
 
 
 def validate(veld_metadata):
-    with open("./veld_schema.json", "r") as schema_file:
-        schema_dict = json.load(schema_file)
-        try:
-            jsonschema.validate(instance=veld_metadata, schema=schema_dict)
-            print("valid.")
-        except jsonschema.exceptions.ValidationError as err:
-            print("invalid")
-            raise err
-    
+    try:
+        jsonschema.validate(instance=veld_metadata, schema=SCHEMA_DICT)
+        print("valid.")
+    except jsonschema.exceptions.ValidationError as err:
+        print("invalid")
+        raise err
+
+
 if __name__ == "__main__":
     validate()
-    
-
