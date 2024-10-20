0c60a4055d6352a29c06061e877b2a67284c06c7
SteffRhes <steffrhes@pm.me>
2024-10-20 10:39:22 +0200

    wip: test schema data structure works with validator

--- veld_spec.py
+++ veld_spec.py
@@ -108,6 +108,7 @@ root = NodeDisjunction(
                                             ),
                                             target=Node(
                                                 is_optional=True,
+                                                is_variable=True,
                                                 content=None
                                             )
                                         ),
@@ -119,11 +120,13 @@ root = NodeDisjunction(
                                                 is_optional=True,
                                                 content=[
                                                     Node(
-                                                        content=None
+                                                        content=None,
+                                                        is_variable=True,
                                                     ),
                                                     NodeList(
                                                         content=Node(
-                                                            content=None
+                                                            content=None,
+                                                            is_variable=True,
                                                         ),
                                                     )
                                                 ]
@@ -136,7 +139,8 @@ root = NodeDisjunction(
                                             ),
                                             target=Node(
                                                 is_optional=True,
-                                                content=None
+                                                is_variable=True,
+                                                content="<ADDITIONAL>"
                                             )
                                         ),
                                     ]
@@ -152,14 +156,22 @@ root = NodeDisjunction(
                     target=NodeDict(
                         content=[
                             NodeMapping(
-                                content=None,
+                                content=Node(
+                                    content="<VELD_SERVICE_NAME>",
+                                    is_variable=True,
+                                ),
                                 target=NodeDict(
                                     content=[
                                         NodeMapping(
+                                            is_optional=True,
                                             content=Node(
-                                                content="compose_definition"
+                                                content="<DOCKER_COMPOSE_DEFINITION>",
+                                                is_variable=True,
                                             ),
-                                            target=None,
+                                            target=Node(
+                                                content="<DOCKER_COMPOSE_DEFINITION_TARGET>",
+                                                is_variable=True,
+                                            ),
                                         ),
                                         NodeMapping(
                                             content=Node(
@@ -170,7 +182,14 @@ root = NodeDisjunction(
                                                 content=NodeDict(
                                                     content=[
                                                         NodeMapping(
-                                                            content=None,
+                                                            content=Node(
+                                                                content="<HOST_PATH>",
+                                                                is_variable=True,
+                                                            ),
+                                                            target=Node(
+                                                                content="<CONTAINER_PATH>",
+                                                                is_variable=True,
+                                                            )
                                                         )
                                                     ]
                                                 ),
@@ -188,9 +207,6 @@ root = NodeDisjunction(
 )
 
 
-DEBUG = False
-DEBUG_RETURN = False
-
 
 def validate(obj_to_validate, node: Node, path=""):
     
@@ -204,27 +220,47 @@ def validate(obj_to_validate, node: Node, path=""):
                 is_one_valid = True
         if not is_one_valid:
             all_errors = "; ".join(r[1] for r in result_list)
-            return (False, f"all possilbe options are invalid: ({all_errors})")
+            return (False, f"all possible options are invalid: ({all_errors})")
         else:
             return (True, None)
     
     def handle_node_dict(obj_to_validate, node: NodeDict, path):
+        
+        def go_to_target(obj_value, target: Node, path):
+            if obj_value is not None:
+                result = validate(obj_value, target, path_sub)
+                if not result[0]:
+                    return result
+            elif not target.is_optional:
+                return (False, f"non-optional value missing at: {path_sub}/")
+            return (True, None)
+        
         if type(obj_to_validate) is not dict:
             return (False, f"is not dict at: {path}/")
         else:
+            node_keys_variables = []
             for node_mapping in node.content:
-                if node_mapping.content.content not in obj_to_validate and not node_mapping.is_optional:
-                    return (False, f"non-optional key missing: '{node_mapping.content.content}', at: {path}/")
+                node_key = node_mapping.content.content
+                node_target = node_mapping.target
+                if node_mapping.content.is_variable:
+                    node_keys_variables.append(node_mapping)
+                    continue
+                if node_key not in obj_to_validate and not node_mapping.is_optional:
+                    return (False, f"non-optional key missing: '{node_key}', at: {path}/")
                 else:
-                    node_key = node_mapping.content.content
                     obj_value = obj_to_validate.pop(node_key, None)
                     path_sub = path + "/" + node_key
-                    if obj_value is not None:
-                        result = validate(obj_value, node_mapping.target, path_sub)
-                        if not result[0]:
-                            return result
-                    elif not node_mapping.target.is_optional:
-                        return (False, f"non-optional value missing at: {path_sub}/")
+                    result = go_to_target(obj_value, node_target, path_sub)
+                    if not result[0]:
+                        return result
+            for node_mapping in node_keys_variables:
+                node_target = node_mapping.target
+                obj_key = next(iter(obj_to_validate.keys()))
+                obj_value = obj_to_validate.pop(obj_key)
+                path_sub = path + "/" + obj_key
+                result = go_to_target(obj_value, node_target, path_sub)
+                if not result[0]:
+                    return result
             if len(obj_to_validate) != 0:
                 unmatched_keys = ",".join(k for k in obj_to_validate.keys())
                 return (False, f"elements not matching anything at: {path + '/' + unmatched_keys}")
@@ -250,6 +286,8 @@ def validate(obj_to_validate, node: Node, path=""):
             return (False, f"is not primitive type, but {obj_type}, at: {path}/")
         elif obj_type is None and not node.is_optional:
             return (False, f"non-optional value is empty at: {path}/")
+        # elif node.is_variable and not (node.content.startswith("<") and node.content.endswith(">")):
+        #     return (False, f"node is variable, but has fixed content: {node.content},at {path}/")
         return (True, None)
     
     def validate_main(obj_to_validate, node: Node, path):
@@ -313,7 +351,8 @@ test_dict_4 = {
 test_dict_5 = {
     "x-veld": {
         "code": {
-            "description"
+            "description": None,
+            "topics": None,
         }
     },
     "services": {
@@ -328,10 +367,10 @@ test_dict_5 = {
 }
 
 
-print(validate(test_dict_1, root))
-print(validate(test_dict_2, root))
-print(validate(test_dict_3, root))
-print(validate(test_dict_4, root))
+# print(validate(test_dict_1, root))
+# print(validate(test_dict_2, root))
+# print(validate(test_dict_3, root))
+# print(validate(test_dict_4, root))
 print(validate(test_dict_5, root))
 
 pass
@@ -457,7 +496,7 @@ pass
 #         pass
 #
 #     return validate_main()
-    
-
-if __name__ == "__main__":
-    validate({})
+#
+#
+# if __name__ == "__main__":
+#     validate({})
