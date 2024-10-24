1b6d1eaa5c2fb6d9aecf6321a2f284b37a0ce1ef
SteffRhes <steffrhes@pm.me>
2024-10-24 19:04:39 +0200

    update tests to newer schema

--- tests/test.py
+++ tests/test.py
@@ -11,6 +11,16 @@ TEST_FILES_PATH = "tests/veld_yaml_files/"
 
 class TestVeldMetadata(unittest.TestCase):
     
+    def test_chain_barebone_valid(self):
+        result = validate(yaml_to_validate="./tests/veld_yaml_files/chain_barebone_valid.yaml")
+        print(result[1])
+        self.assertTrue(result[0])
+    
+    def test_chain_barebone_invalid(self):
+        result = validate(yaml_to_validate="./tests/veld_yaml_files/chain_barebone_invalid.yaml")
+        print(result[1])
+        self.assertFalse(result[0])
+    
     def test_code_barebone_valid(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/code_barebone_valid.yaml")
         print(result[1])
