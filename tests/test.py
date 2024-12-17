68d70ac820257dec5a8cf7cc8ffa15b7607f22c4
SteffRhes <steffrhes@pm.me>
2024-12-17 13:21:58 +0100

    add production veld testing

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from veld_spec import validate
@@ -7,30 +8,40 @@ class TestVeldMetadata(unittest.TestCase):
     
     def test_chain_barebone_valid(self):
         result = validate(yaml_to_validate="./veld_yaml_files/chain_barebone_valid.yaml")
-        print(result[1])
         self.assertTrue(result[0])
     
     def test_chain_barebone_invalid(self):
         result = validate(yaml_to_validate="./veld_yaml_files/chain_barebone_invalid.yaml")
-        print(result[1])
         self.assertFalse(result[0])
     
     def test_code_barebone_valid(self):
         result = validate(yaml_to_validate="./veld_yaml_files/code_barebone_valid.yaml")
-        print(result[1])
         self.assertTrue(result[0])
     
     def test_code_barebone_invalid(self):
         result = validate(yaml_to_validate="./veld_yaml_files/code_barebone_invalid.yaml")
-        print(result[1])
         self.assertFalse(result[0])
     
     def test_data_barebone_valid(self):
         result = validate(yaml_to_validate="./veld_yaml_files/data_barebone_valid.yaml")
-        print(result[1])
         self.assertTrue(result[0])
     
     def test_data_barebone_invalid(self):
         result = validate(yaml_to_validate="./veld_yaml_files/data_barebone_invalid.yaml")
-        print(result[1])
         self.assertFalse(result[0])
+        
+    def test_production_velds(self):
+        production_velds_folder = "./veld_yaml_files/production_velds/"
+        num_correct = 0
+        num_incorrect = 0
+        for veld_file in os.listdir(production_velds_folder):
+            veld_file_path = production_velds_folder + "/" + veld_file
+            result = validate(yaml_to_validate=veld_file_path)
+            if not result[0]:
+                num_incorrect += 1
+                print(veld_file_path)
+                print(result[1])
+            else:
+                num_correct += 1
+        print("num_correct:", num_correct)
+        print("num_incorrect:", num_incorrect)
