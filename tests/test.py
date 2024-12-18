d15b9f322b9989e641bdd0f0aeae6419539a3ed0
SteffRhes <steffrhes@pm.me>
2024-12-18 17:33:31 +0100

    update test files

--- tests/test.py
+++ tests/test.py
@@ -12,14 +12,20 @@ class TestVeldMetadata(unittest.TestCase):
     
     def test_chain_barebone_valid(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/chain_barebone_valid.yaml")
+        if not result[0]:
+            print(result[1])
         self.assertTrue(result[0])
     
     def test_chain_example_readme_1(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/chain_example_readme_1.yaml")
+        if not result[0]:
+            print(result[1])
         self.assertTrue(result[0])
     
     def test_chain_example_readme_2(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/chain_example_readme_2.yaml")
+        if not result[0]:
+            print(result[1])
         self.assertTrue(result[0])
     
     def test_code_barebone_invalid(self):
@@ -28,14 +34,20 @@ class TestVeldMetadata(unittest.TestCase):
     
     def test_code_barebone_valid(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/code_barebone_valid.yaml")
+        if not result[0]:
+            print(result[1])
         self.assertTrue(result[0])
     
     def test_code_example_readme_1(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/code_example_readme_1.yaml")
+        if not result[0]:
+            print(result[1])
         self.assertTrue(result[0])
     
     def test_code_example_readme_2(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/code_example_readme_2.yaml")
+        if not result[0]:
+            print(result[1])
         self.assertTrue(result[0])
     
     def test_data_barebone_invalid(self):
@@ -44,14 +56,20 @@ class TestVeldMetadata(unittest.TestCase):
     
     def test_data_barebone_valid(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/data_barebone_valid.yaml")
+        if not result[0]:
+            print(result[1])
         self.assertTrue(result[0])
     
     def test_data_example_readme_1(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/data_example_readme_1.yaml")
+        if not result[0]:
+            print(result[1])
         self.assertTrue(result[0])
     
     def test_data_example_readme_2(self):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/data_example_readme_2.yaml")
+        if not result[0]:
+            print(result[1])
         self.assertTrue(result[0])
         
     def test_production_velds(self):
