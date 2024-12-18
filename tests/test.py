201ace0e941bf1796aa3092f6b34c6a3e0d1ef0d
SteffRhes <steffrhes@pm.me>
2024-12-18 14:30:46 +0100

    reactivate production veld tests

--- tests/test.py
+++ tests/test.py
@@ -54,22 +54,22 @@ class TestVeldMetadata(unittest.TestCase):
         result = validate(yaml_to_validate="./tests/veld_yaml_files/data_example_readme_2.yaml")
         self.assertTrue(result[0])
         
-    # def test_production_velds(self):
-    #     production_velds_folder = "./tests/veld_yaml_files/production_velds/"
-    #     num_correct = 0
-    #     num_incorrect = 0
-    #     for veld_file in os.listdir(production_velds_folder):
-    #         veld_file_path = production_velds_folder + "/" + veld_file
-    #         result = validate(yaml_to_validate=veld_file_path)
-    #         if not result[0]:
-    #             num_incorrect += 1
-    #             print(veld_file_path)
-    #             print(result[1])
-    #         else:
-    #             num_correct += 1
-    #     print("num_correct:", num_correct)
-    #     print("num_incorrect:", num_incorrect)
-    #     self.assertTrue(num_incorrect == 0)
+    def test_production_velds(self):
+        production_velds_folder = "./tests/veld_yaml_files/production_velds/"
+        num_correct = 0
+        num_incorrect = 0
+        for veld_file in os.listdir(production_velds_folder):
+            veld_file_path = production_velds_folder + "/" + veld_file
+            result = validate(yaml_to_validate=veld_file_path)
+            if not result[0]:
+                num_incorrect += 1
+                print(veld_file_path)
+                print(result[1])
+            else:
+                num_correct += 1
+        print("num_correct:", num_correct)
+        print("num_incorrect:", num_incorrect)
+        self.assertTrue(num_incorrect == 0)
 
 
 if __name__ == "__main__":
